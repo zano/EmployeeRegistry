651d23272ebfb59442373a1a46e77a303fa9876d
Adrian Lozano <adrian@xenotype.com>
2016-05-11 16:50:17 +0200

    Add comment about upcoming Company refactoring

--- EmployeeRegistry/Models/Employee.cs
+++ EmployeeRegistry/Models/Employee.cs
@@ -13,6 +13,8 @@ namespace EmployeeRegistry.Models
         public int Salary { get; set; }
         public string Position { get; set; }
         public string Department { get; set; }
-        public string Company { get; set; }
+        public string Company { get; set; } 
+
+        // Todo: Extract Company to own entity
     }
 }
